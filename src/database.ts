8f9ccf0f477b2eefb8575e47ea24394fb8bf5445
LDK <lemondropkid@gmail.com>
2023-06-22 21:44:09 -0700

    Use DATABASE_URL environment variable if available

--- src/database.ts
+++ src/database.ts
@@ -5,17 +5,22 @@ let dataSource: DataSource | null = null;
 
 export const getDataSource = async () => {
   if (!dataSource) {
+    let databaseUrl = process.env.DATABASE_URL;
+    if (!databaseUrl) {
+      databaseUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+    }
+
     dataSource = new DataSource({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
+      url: databaseUrl,
       entities: [
         "src/entity/**/*.ts"
       ],
       synchronize: true,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      extra: {
+        ssl: process.env.NODE_ENV === 'production' ? true : false
+      }
     });
 
     await dataSource.initialize();
