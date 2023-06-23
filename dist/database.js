b52c24313aba724e5781a427d7a1e171fdc5baad
LDK <lemondropkid@gmail.com>
2023-06-22 22:15:45 -0700

    Build of last commit

--- dist/database.js
+++ dist/database.js
@@ -4,12 +4,15 @@ exports.getDataSource = void 0;
 const typeorm_1 = require("typeorm");
 let dataSource = null;
 const getDataSource = async () => {
-    console.log('process env', process.env);
     if (!dataSource) {
         let databaseUrl = process.env.DATABASE_URL;
-        // if (!databaseUrl) {
-        //   databaseUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-        // }
+        let useSSL = false;
+        if (!databaseUrl) {
+            databaseUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+        }
+        else {
+            useSSL = true;
+        }
         dataSource = new typeorm_1.DataSource({
             type: 'postgres',
             url: databaseUrl,
@@ -17,10 +20,14 @@ const getDataSource = async () => {
                 "src/entity/**/*.ts"
             ],
             synchronize: true,
-            ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
-            extra: {
-                ssl: process.env.NODE_ENV === 'production' ? true : false
-            }
+            ssl: useSSL ? {
+                rejectUnauthorized: false,
+            } : undefined,
+            extra: useSSL ? {
+                ssl: {
+                    rejectUnauthorized: false,
+                },
+            } : undefined,
         });
         await dataSource.initialize();
     }
