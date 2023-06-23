9fa09e8f2a240d8ad29f85d0b8954ec2b8547e0b
LDK <lemondropkid@gmail.com>
2023-06-22 22:03:19 -0700

    Remove some console logs

--- src/database.ts
+++ src/database.ts
@@ -3,19 +3,14 @@ import { DataSource } from 'typeorm';
 let dataSource: DataSource | null = null;
 
 export const getDataSource = async () => {
-
-  console.log('process env', process.env);
-
   if (!dataSource) {
     let databaseUrl = process.env.DATABASE_URL;
-
-    console.log('database url, try 1', databaseUrl);
 
     if (!databaseUrl) {
       databaseUrl = `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
     }
 
-    console.log('database url, try 2', databaseUrl);
+    console.log('database url', databaseUrl);
 
     dataSource = new DataSource({
       type: 'postgres',
