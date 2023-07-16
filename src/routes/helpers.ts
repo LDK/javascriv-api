4f957ad15c6b4c066445b20920eaaaf505485a62
LDK <lemondropkid@gmail.com>
2023-07-16 15:49:48 -0700

    Send new user e-mail on registration

--- src/routes/helpers.ts
+++ src/routes/helpers.ts
@@ -1,63 +1,152 @@
 import { Repository } from "typeorm";
 import { Project } from "../entity/Project";
-import { ProjectFile } from "../shm/project-types/ProjectTypes";
 import { ProjectTreeFile } from "./projects";
 import { File } from "../entity/File";
+import { User } from "../entity/User";
+
+type CleanUser = Omit<User, 'passwordHash'>;
+type ProjectFile = ProjectTreeFile & { 
+  changed?: boolean;
+  creator: CleanUser;
+  lastEditor: CleanUser;
+  editing?: CleanUser;
+};
 
 // Helper function to save a file and its children recursively
-export async function saveFile(file:ProjectFile, parentPath:string | null, project:Project, fileRepository:Repository<File>) {
+export async function saveFile(file:ProjectTreeFile, parent: File | undefined, project:Project, fileRepository:Repository<File>, user:User) {
+  const { children: parentChildren, ...parentFile } = parent ? parent as ProjectTreeFile : { children: undefined };
+
   const newFile = new File();
   newFile.type = file.type;
   newFile.name = file.name;
   newFile.path = file.path;
-  newFile.parent = parentPath;
+  newFile.parent = parent;
   newFile.subType = file.subType;
-  newFile.attachment = file.attachment;
   newFile.content = file.content;
-  newFile.initialContent = file.initialContent;
   newFile.project = project;
+  newFile.projectId = project.id;
+  newFile.lastEdited = new Date();
+  newFile.lastEditor = user;
+  if (!file.creator) {
+    newFile.creator = user;
+  }
+  newFile.editing = undefined;
+
+  console.log('saveFile saving file:', newFile);
 
   await fileRepository.save(newFile);
+
+  console.log('saveFile saved file:', newFile);
 
   // Recursively save children if they exist
   if (file.children) {
     for (const childFile of file.children) {
-      await saveFile(childFile, file.path, project, fileRepository);
+      await saveFile(childFile, newFile, project, fileRepository, user);
     }
   }
+
+  // Convert newFile to a ProjectTreeFile called returnFile
+  const returnFile:ProjectTreeFile = {
+    id: newFile.id,
+    type: newFile.type,
+    name: newFile.name,
+    path: newFile.path,
+    parent: newFile.parent,
+    subType: newFile.subType,
+    content: newFile.content,
+    creator: newFile.creator,
+    lastEdited: newFile.lastEdited,
+    lastEditor: newFile.lastEditor,
+    editing: newFile.editing,
+    project: newFile.project,
+    projectId: newFile.projectId
+  }
+
+  return returnFile;
 }
 
 export interface FileIndex {
-  [path:string]: ProjectTreeFile;
+  [id:number]: ProjectTreeFile;
 }
 
-export const buildHierarchicalFiles = (files:ProjectFile[]) => {
-  // The hierarchical files object we will build
-  let hierarchicalFiles = [];
+export const buildHierarchicalFiles = (files:ProjectTreeFile[]) => {
+  let hierarchicalFiles:ProjectFile[] = [];
+  const idToFile:FileIndex = {};
 
-  // Maps each path to the corresponding file object
-  const pathToFile:FileIndex = {};
-
-  // Populate the initial mapping from paths to file objects
   for (const file of files) {
-    pathToFile[file.path] = file as ProjectTreeFile;
+    idToFile[file.id] = file;
+
+    if (!file.parent) {
+      const pFile = file as ProjectFile;
+      hierarchicalFiles.push(pFile);
+    }
   }
 
-  // Now we go through each file, and assign it as a child to its parent in the mapping
-  for (const file of (files as ProjectTreeFile[])) {
-    if (file.parent) {
-      // The parent file is in the mapping
-      const parent = pathToFile[file.parent];
+  let hasChanges = true;
 
-      // Add the file to the parent's children
-      if (!parent.children) parent.children = [];
-      parent.children.push(file);
-    } else {
-      // If the file has no parent, it's a top-level file
-      hierarchicalFiles.push(file);
+  while (hasChanges) {
+    hasChanges = false;
+
+    for (const file of files) {
+      if (file.parent && file.parent.id) {
+        const parent = idToFile[file.parent.id];
+
+        if (parent && (!parent.children || !parent.children.includes(file))) {
+          if (!parent.children) {
+            parent.children = [];
+          }
+
+          delete file.parent;
+
+          parent.children.push(file);
+          hasChanges = true;
+        }
+      }
     }
   }
 
   return hierarchicalFiles;
+}
+
+/**
+ * Compares two objects deeply and returns true if they are equal.
+ *
+ * @param obj1 The first object to compare.
+ * @param obj2 The second object to compare.
+ *
+ * @returns {boolean | undefined} Returns true if the objects are equal, false if they are not. 
+ * If one or both of the inputs are not objects, returns undefined.
+ */
+
+export const areObjectsEqual = (obj1: any, obj2: any) => {
+  if (typeof obj1 !== 'object' || typeof obj2 !== 'object') {
+    return undefined;
+  }
+
+  const keys1 = Object.keys(obj1).sort();
+  const keys2 = Object.keys(obj2).sort();
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < keys1.length; i++) {
+    if (keys1[i] !== keys2[i]) {
+      return false;
+    }
+
+    const val1 = obj1[keys1[i]];
+    const val2 = obj2[keys2[i]];
+
+    if (typeof val1 === 'object' && typeof val2 === 'object') {
+      if (!areObjectsEqual(val1, val2)) {
+        return false;
+      }
+    } else if (val1 !== val2) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,6 +3,7 @@ import { User } from '../entity/User';
 import bcrypt from 'bcrypt';
 import { getDataSource } from '../database';
 import jwt from 'jsonwebtoken';
+import Mailgun from 'mailgun-js';
 
 const router = Router();
 
@@ -11,6 +12,10 @@ router.post('/user/register', async (req, res) => {
 
   if (!username || !password || !email) {
     return res.status(400).send('Missing username, e-mail or password');
+  }
+
+  if (username.includes('@')) {
+    return res.status(400).send('Username cannot contain @ symbol.');
   }
 
   // Get the User Repository from the DataSource
@@ -31,7 +36,7 @@ router.post('/user/register', async (req, res) => {
 
   const passwordHash = await bcrypt.hash(password, 10);
 
-  const user = userRepository.create({ username, email, passwordHash });
+  const user:User = userRepository.create({ username, email, passwordHash });
 
   await userRepository.save(user);
 
@@ -39,6 +44,50 @@ router.post('/user/register', async (req, res) => {
 
   // Signing a JWT using the secret key from the environment variables
   const token = jwt.sign({ id: newUser.id, username: newUser.username }, process.env.SECRET_KEY as string, { expiresIn: '720h' });
+
+  const mailService = process.env.MAIL_SERVICE as string;
+
+  switch (mailService) {
+    case 'mailgun':
+      // Initialize mailgun
+      const mailgun = Mailgun({ apiKey: process.env.MAILGUN_API_KEY as string, domain: process.env.MAILGUN_DOMAIN as string });
+
+      const data = {
+        from: 'javaScriv <lemondropkid@gmail.com>',
+        to: email,
+        subject: 'Welcome to Our App',
+        text: 'Thank you for registering at our app. We are glad to have you!'
+      };
+    
+      mailgun.messages().send(data, (error, body) => {
+        if (error) {
+          console.error('Failed to send email', error);
+        } else {
+          console.log('Email sent', body);
+        }
+      });
+    
+    break;
+
+    case 'postmark':
+      // Initialize postmark
+      // Require:
+      var postmark = require("postmark");
+
+      // Send an email:
+      var client = new postmark.ServerClient(process.env.POSTMARK_API_KEY as string);
+
+      client.sendEmail({
+        "From": "javaScriv@electric-bungalow.com",
+        "To": email,
+        "Subject": "Welcome to javaScriv!  Verify your account.",
+        "HtmlBody": "<strong>Hello</strong> dear Postmark user.",
+        "TextBody": "Hello from Postmark!",
+        "MessageStream": "outbound"
+      });
+
+    break;
+  }
 
   return res.status(201).send({...newUser, token});
 });
